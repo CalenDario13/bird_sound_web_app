0da26f5fc10c8e6424ad3322a10e304f682220f1
CalenDario13 <dariorusso.dr13@gmail.com>
2021-07-12 23:19:19 +0200

    Trying to create the button

--- webapp/script/main.js
+++ webapp/script/main.js
@@ -1,4 +1,30 @@
+API_ENDPOINT = "https://fn5zu5vj91.execute-api.us-east-1.amazonaws.com/bird-web-app"
+
+
 function showMenu() {
         document.getElementById("mySidebar").style.width = "250px";
         document.getElementById("main").style.marginLeft = "250px";
 }
+
+document.getElementById("inp"){
+
+
+}
+
+
+
+
+let audio = document.getElementById("inp").files[0];
+let formData = new FormData();
+formData.append("audio", audio);
+$.ajax({
+        url: API_ENDPOINT,
+        type: 'POST',
+        crossDomain: true,
+        data: JSON.stringify(formData),
+        dataType: 'json',
+        contentType: "application/json",
+        success: function (response) {
+        document.getElementById("genreReturned").textContent = response;
+        },
+});
