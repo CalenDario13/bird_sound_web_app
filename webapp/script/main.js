0afa71b2bb1f3df4ac1f05d6798b83456296a71c
CalenDario13 <dariorusso.dr13@gmail.com>
2021-07-16 18:28:55 +0200

    Web site with play button (not working)

--- webapp/script/main.js
+++ webapp/script/main.js
@@ -1,30 +1,39 @@
-API_ENDPOINT = "https://fn5zu5vj91.execute-api.us-east-1.amazonaws.com/bird-web-app"
+API_ENDPOINT = "https://uo8jn9b563.execute-api.us-east-1.amazonaws.com/default"
 
 
-function showMenu() {
-        document.getElementById("mySidebar").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-}
-
-document.getElementById("inp"){
-
-
-}
-
-
+document.getElementById('inp').onchange = function(event) {
+    var mybird = Audio()
+    var myaudio = document.getElementById('player');
+    myaudio.src = URL.createObjectURL(event.target.files[0]); 
+  };
+  
+document.getElementById('play').onclick = function(){
+    document.getElementById('player').play();
+};
 
 
-let audio = document.getElementById("inp").files[0];
-let formData = new FormData();
-formData.append("audio", audio);
-$.ajax({
-        url: API_ENDPOINT,
-        type: 'POST',
-        crossDomain: true,
-        data: JSON.stringify(formData),
-        dataType: 'json',
-        contentType: "application/json",
-        success: function (response) {
+document.getElementById('player').ontimeupdate = function() {
+    var bar = document.getElementById('seekbar');
+    var sound = document.getElementById('player');
+    var dur = sound.duration;
+    bar.value = sound.currentTime / dur;
+};
+
+ 
+document.getElementById("genre").onclick = function(){
+    var sound = document.getElementById("player")
+    var mysound = sound.toDataURL()
+	  var inputData = {"data": mysound};
+ 
+    $.ajax({
+      url: API_ENDPOINT,
+      type: 'POST',
+      crossDomain: true,
+      data: JSON.stringify(inputData),
+      dataType: 'json',
+      contentType: "application/json",
+      success: function (response) {
         document.getElementById("genreReturned").textContent = response;
-        },
-});
+      },
+  });
+}
