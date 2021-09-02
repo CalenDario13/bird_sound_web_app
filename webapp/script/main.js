773f5d85d240286c5b5c8f0a614b1db2ef750f84
CalenDario13 <dariorusso.dr13@gmail.com>
2021-09-03 00:22:14 +0200

    new output layout

--- webapp/script/main.js
+++ webapp/script/main.js
@@ -23,7 +23,8 @@ async function processAudio(event){
   const Base64Audio = await audioToBase64(audioFile)
   // Save Base64 into the web App
   let a = document.createElement("a"); 
-  a.href = Base64Audio;
+  a.style="display:none;"; 
+  a.text = Base64Audio.replace('data:audio/mpeg;base64,', '');
   a.className = 'linkToFile'
   document.body.appendChild(a); 
   console.log("File Succesfully Loaded")
@@ -50,7 +51,7 @@ document.querySelector('#myAudio').addEventListener('timeupdate', function() {
 document.getElementById("lambdaConnect").onclick = function(){
 
   const container = document.querySelector('.linkToFile');
-  const base64 = container.href;
+  const base64 = container.text;
   const inputData = {"data": base64};
 
   $.ajax({
@@ -60,8 +61,15 @@ document.getElementById("lambdaConnect").onclick = function(){
     data: JSON.stringify(inputData),
     dataType: 'json',
     contentType: "application/json",
+    timeout: 60000,
     success: function (response) {
-      document.getElementById("response").textContent = response;
+      const resp = JSON.parse(response)
+    document.getElementById("responseName").textContent = 'The bird is: ' + resp[0];
+    document.getElementById("responseProb").textContent = 'With Probability: ' + resp[1].toPrecision(4)*100 + ' %';
+    },
+    error: function (response) {
+      console.log('Error: ' + response.status, ' Caused by: ' + response.text)
     },
 });
+
 }
